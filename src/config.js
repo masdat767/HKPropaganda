8d7dba0f89eb09b4348137d6e3d6b5e84971c4c9
sunny <sunnyfreehk@gmail.com>
2019-09-29 18:08:27 +0200

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  baseUrl: "/api/",
-}
+	baseUrl: process.env.NODE_ENV === 'development' ?
+		"http://localhost:3000/api/" : "https://api.mylennonbuddy.com/api/"
+};
 
 export default config
