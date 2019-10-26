17aac1b0dc9bd9bc801f130794b89693d3b667ae
sunny <sunnyfreehk@gmail.com>
2019-10-26 03:20:42 +0200

    check login later

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -4,11 +4,11 @@ import { navigate } from "@reach/router"
 import { isLoggedIn } from "../utils/auth"
 
 const PrivateRoute = ({ component: Component, location, ...rest }) => {
-  if (!isLoggedIn() && location.pathname !== `/game/login`) {
-    // If we’re not logged in, redirect to the home page.
-    navigate(`/game/login`)
-    return null
-  }
+  // if (!isLoggedIn() && location.pathname !== `/game/login`) {
+  //   // If we’re not logged in, redirect to the home page.
+  //   navigate(`/game/login`)
+  //   return null
+  // }
 
   return <Component {...rest} />
 }
