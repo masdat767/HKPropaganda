398bafe12ee913e1204cbcacdb827848507c70d0
sunny <sunnyfreehk@gmail.com>
2019-11-26 01:26:15 +0100

    process.env.GOOGLE_SPREADSHEET_URL

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const reportUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://script.google.com/macros/s/AKfycbw1wIyVKpTn0q1qvgjr81nb41N9fff9fEjbFUI34QJkYis5ia0/exec"
-    : "https://script.google.com/macros/s/AKfycbyfu_0T3D6XxIZiz4vwtPni74e0ztE0mGMwtIHGE5iJDS_TLy0/exec"
+	(!process.env.GOOGLE_SPREADSHEET_URL || process.env.GOOGLE_SPREADSHEET_URL === "development")
+    ? "https://script.google.com/macros/s/AKfycbyfu_0T3D6XxIZiz4vwtPni74e0ztE0mGMwtIHGE5iJDS_TLy0/exec"
+    : "https://script.google.com/macros/s/AKfycbw1wIyVKpTn0q1qvgjr81nb41N9fff9fEjbFUI34QJkYis5ia0/exec"
 
 const config = {
   baseUrl: "https://api-dev.mylennonbuddy.com/api/",
