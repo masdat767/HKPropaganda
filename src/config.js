e77ee996d72e631d53f4d3d0fb89b4a4de7c4494
sunny <sunnyfreehk@gmail.com>
2019-11-26 01:29:41 +0100

    giveup

--- src/config.js
+++ src/config.js
@@ -1,12 +1,15 @@
-const reportUrl =
-	(!process.env.GOOGLE_SPREADSHEET_URL || process.env.GOOGLE_SPREADSHEET_URL === "development")
-    ? "https://script.google.com/macros/s/AKfycbyfu_0T3D6XxIZiz4vwtPni74e0ztE0mGMwtIHGE5iJDS_TLy0/exec"
-    : "https://script.google.com/macros/s/AKfycbw1wIyVKpTn0q1qvgjr81nb41N9fff9fEjbFUI34QJkYis5ia0/exec"
-
+// prod
+// const config = {
+//   baseUrl: "https://api.mylennonbuddy.com/api/",
+//   authUrl: "https://api.mylennonbuddy.com/auth/google",
+//   reportUrl: "https://script.google.com/macros/s/AKfycbw1wIyVKpTn0q1qvgjr81nb41N9fff9fEjbFUI34QJkYis5ia0/exec"
+// }
+     
+// dev
 const config = {
   baseUrl: "https://api-dev.mylennonbuddy.com/api/",
   authUrl: "https://api-dev.mylennonbuddy.com/auth/google",
-  reportUrl,
+  reportUrl: "https://script.google.com/macros/s/AKfycbyfu_0T3D6XxIZiz4vwtPni74e0ztE0mGMwtIHGE5iJDS_TLy0/exec"
 }
 
 export default config
